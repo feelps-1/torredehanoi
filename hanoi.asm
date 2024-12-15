73bb9be8ae76cba7a6d6003009e53d663fb93528
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-12-15 18:32:30 -0300

    reorganização de funções

--- hanoi.asm
+++ hanoi.asm
@@ -1,4 +1,22 @@
-; Agradecimentos: https://mentebinaria.gitbook.io/assembly/a-base/registradores-de-proposito-geral
+; Declaração de dados: 
+section .data                         ; Usada para armazenar dados inicializados do programa, por exemplo uma variável global.
+
+    ; Definindo a mensagem
+    msg:
+                          db        "Disco: "   
+        disco:            db        " "
+                          db        "   "                      
+        torre_saida:      db        " "  
+                          db        " -> "     
+        torre_ida:        db        " ", 0xa  ; para quebrar linha
+        
+        move_msg_length            equ       $-msg
+
+    erro_msg db "Erro: Numero invalido ou fora do limite.", 0xa
+    erro_len equ $ - erro_msg
+
+    ask_for_disks db "Escolha um número de discos de 0 a 99: " 
+    ask_len equ $ - ask_for_disks
 
 section .bss                             ; Dados não inicializados
    num resb 3                            ; Reserva 3 bytes para armazenar os dígitos + \n
@@ -8,53 +26,62 @@ section .text                            ; Código executável
     global _start
 
 _start:
-    ; Configurar leitura
-    mov eax, 3                           ; Chamada de sistema 'read'
-    mov ebx, 0                           ; stdin
-    mov ecx, num                         ; Endereço de armazenamento
-    mov edx, 3                           ; Número máximo de bytes lidos (2 dígitos + \n)
-    int 80h                              ; Chama interrupção de leitura
 
-    ; Carregar o primeiro dígito
-    movzx eax, byte [num]                ; Primeiro dígito (ASCII)
+    begin:
+        mov ecx, ask_for_disks
+        mov edx, ask_len
+        call print
+
+        ; Configurar leitura
+        mov eax, 3                           ; Chamada de sistema 'read'
+        mov ebx, 0                           ; stdin
+        mov ecx, num                         ; Endereço de armazenamento
+        mov edx, 3                           ; Número máximo de bytes lidos (2 dígitos + \n)
+        int 80h                              ; Chama interrupção de leitura
+
+        ; Carregar o primeiro dígito
+        movzx eax, byte [num]                ; Primeiro dígito (ASCII)
+        call convert_to_int            
+        mov ebx, eax                         ; Armazena o primeiro dígito em ebx
+
+        ; Carregar o segundo byte
+        movzx eax, byte [num+1]              ; Segundo byte (ASCII ou \n)
+        cmp eax, 10                         ; Verifica se é o caractere de nova linha (\n)
+        je single_digit                ; Se sim, é número de 1 dígito
+
+        call convert_to_int
+
+        ; Combina os dois dígitos
+        movzx eax, byte [num+2]              ; Segundo byte (ASCII ou \n)
+        cmp eax, 10                         ; Verifica se é o caractere de nova linha (\n)
+        jnz error
+        mov eax, ebx                         ; Primeiro dígito
+        imul eax, 10                         ; Multiplica por 10
+        add eax, ebx                         ; Soma o segundo dígito
+        jmp processar                        ; Continua para o processamento
+
+convert_to_int:
     sub eax, '0'                         ; Converte de ASCII para número
     cmp eax, 0                           ; Verifica se é um número válido
-    jl erro                              ; Se menor que 0, erro
-    cmp eax, 9
-    jg erro                              ; Se maior que 9, erro
-    mov ebx, eax                         ; Armazena o primeiro dígito em ebx
+    jl error                             ; Se menor que 0, erro
+    cmp eax, 9                           ; Se maior que 9, erro
+    jg error  
+    ret
 
-    ; Carregar o segundo byte
-    movzx eax, byte [num+1]              ; Segundo byte (ASCII ou \n)
-    cmp eax, 0xA                         ; Verifica se é o caractere de nova linha (\n)
-    je processar_1_digito                ; Se sim, é número de 1 dígito
-
-    sub eax, '0'                         ; Converte de ASCII para número
-    cmp eax, 0
-    jl erro                              ; Se menor que 0, erro
-    cmp eax, 9
-    jg erro                              ; Se maior que 9, erro
-
-    ; Combina os dois dígitos
-    mov eax, ebx                         ; Primeiro dígito
-    imul eax, 10                         ; Multiplica por 10
-    add eax, ebx                         ; Soma o segundo dígito
-    jmp processar                        ; Continua para o processamento
-
-processar_1_digito:
+single_digit:
     mov eax, ebx                         ; Se for apenas 1 dígito, usa diretamente o valor de ebx
     jmp processar
 
 processar:
     ; Verifica se o número é válido
     cmp eax, 99                          ; Limite de 99 discos
-    ja erro                              ; Se maior que 99, erro
+    ja error                             ; Se maior que 99, erro
 
     ; Empilha os valores para a função hanoi
     push dword 2                         ; Torre Auxiliar
     push dword 3                         ; Torre Destino
     push dword 1                         ; Torre Origem
-    push eax                              ; Número de discos
+    push eax                             ; Número de discos
     call hanoi                           ; Chamando a função hanoi
 
     ; Finalização
@@ -62,23 +89,14 @@ processar:
     xor ebx, ebx                         ; Código de saída 0
     int 80h
 
-erro:
+error:
     mov eax, 4                           ; Chamada de sistema 'write'
     mov ebx, 1                           ; stdout
     mov ecx, erro_msg                    ; Mensagem de erro
     mov edx, erro_len                    ; Tamanho da mensagem
     int 80h
 
-    mov eax, 1                           ; Chamada de sistema 'exit'
-    xor ebx, ebx                         ; Código de saída 0
-    int 80h
-
-section .data
-erro_msg db "Erro: Numero invalido ou fora do limite.", 0xa
-erro_len equ $ - erro_msg
-
-
-
+    jmp begin
 
 ; Labels que serão usados durante o funcionamento do codigo
     hanoi: 
@@ -89,7 +107,7 @@ erro_len equ $ - erro_msg
         ;[ebp+20] = Torre de destino
 
         push ebp                      ; empurra o registrador ebp na pilha (para ser a base)
-        mov ebp,esp                   ; aponta o ponteiro do topo da pilha (esp) para a base
+        mov ebp, esp                  ; aponta o ponteiro do topo da pilha (esp) para a base
 
         mov eax, [ebp+8]              ; move para o registrador ax o numero de discos na Torre de origem
         
@@ -106,8 +124,7 @@ erro_len equ $ - erro_msg
         
         call hanoi                    ; chama a label hanoi para guardar a linha de retorno (recursao)
         
-        add esp,16                    ; após retornar da chamada da label hanoi, remove o "lixo" da pilha que está ocupando espaço na memoria
-
+        add esp, 16                   ; após retornar da chamada da label hanoi, remove o "lixo" da pilha que está ocupando espaço na memoria
 
         ; Printando os movimentos
         push dword [ebp+16]           ; empilha o torre de Saida
@@ -117,7 +134,6 @@ erro_len equ $ - erro_msg
         call imprime                  ; chama a label para imprimir os movimentos
         
         add esp, 12                   ; após retornar da chamada da label hanoi, remove o "lixo" da pilha que está ocupando espaço na memoria
-        
         
         ; Segunda recursividade
         push dword [ebp+12]           ; Empurra a torre Origem
@@ -154,26 +170,16 @@ erro_len equ $ - erro_msg
         add al, 64                    ; conversao na tabela ASCII
         mov [torre_ida], al           ; coloca o valor no [torre_ida] para o print
 
-        mov edx, lenght               ; tamanho da mensagem
+        mov edx, move_msg_length      ; tamanho da mensagem
         mov ecx, msg                  ; mensagem em si
-        mov ebx, 1                    ; dá permissão para a saida
-        mov eax, 4                    ; informa que será uma escrita
-        int 128                       ; Interrupção para kernel
+        call print
 
-        mov     esp, ebp              ; aponta o ponteiro da base da pilha (ebp) para o topo
-        pop     ebp                   ; tira o elemento do topo da pilha e guarda o valor em ebp
-        ret                           ; retira o ultimo valor do topo da pilha e da um jump para ele (a linha de retorno nesse caso)
+        call desempilhar             
+        ret                       
 
-; Declaração de dados: 
-section .data                         ; Usada para armazenar dados inicializados do programa, por exemplo uma variável global.
+    print:
+        mov ebx, 1
+        mov eax, 4
+        int 80h
 
-    ; Definindo a mensagem
-    msg:
-                          db        "disc: "   
-        disco:            db        " "
-                          db        "   "                      
-        torre_saida:      db        " "  
-                          db        " -> "     
-        torre_ida:     db        " ", 0xa  ; para quebrar linha
-        
-        lenght            equ       $-msg
+        ret
